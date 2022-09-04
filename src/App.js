906af96e40f4511992a8b4cfccb8ebab01d28076
MD AL JABIR <aljabir098@gmail.com>
2022-09-05 01:37:43 +0600

    somethind add

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import "./Style/main.style.scss";
+import Directory from "./components/directory/directory.component";
+
 const App = () => {
   const categories = [
     {
@@ -27,24 +28,8 @@ const App = () => {
       imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
     },
   ];
-  return (
-    <div className="categories-container">
-      {categories.map(({ id, title, imageUrl }) => {
-        return (
-          <div className="category-container" key={id}>
-            <div
-              className="background-image"
-              style={{ backgroundImage: `url(${imageUrl})` }}
-            />
-            <div className="category-body-container">
-              <h2>{title}</h2>
-              <p>Shop Now</p>
-            </div>
-          </div>
-        );
-      })}
-    </div>
-  );
+
+  return <Directory categories={categories} />;
 };
 
 export default App;
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,15 @@
+import CategoryItem from "../directory-item/category-item.component";
+
+import "../directory/directory.style.scss";
+
+const Directory = ({ categories }) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
