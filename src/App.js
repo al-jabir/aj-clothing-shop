53949199e1895ec27ce496c362a606b8f77bb95c
MD AL JABIR <aljabir098@gmail.com>
2022-09-05 21:23:42 +0600

    routes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,7 @@
-import Directory from "./components/directory/directory.component";
+import Home from "./routes/home";
 
 const App = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl:
-        "https://images.pexels.com/photos/101537/baby-boy-hat-covered-101537.jpeg?cs=srgb&dl=pexels-pixabay-101537.jpg&fm=jpg",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl:
-        "https://images.pexels.com/photos/2300334/pexels-photo-2300334.jpeg?cs=srgb&dl=pexels-adrian-dorobantu-2300334.jpg&fm=jpg",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl:
-        "https://images.pexels.com/photos/1536619/pexels-photo-1536619.jpeg?cs=srgb&dl=pexels-juan-mendez-1536619.jpg&fm=jpg",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl:
-        "https://images.pexels.com/photos/1964970/pexels-photo-1964970.jpeg?cs=srgb&dl=pexels-edgar-serrano-1964970.jpg&fm=jpg",
-    },
-  ];
-
-  return <Directory categories={categories} />;
+  return <Home />;
 };
 
 export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,12 +1,11 @@
 import CategoryItem from "../directory-item/category-item.component";
-
 import "../directory/directory.style.scss";
 
 const Directory = ({ categories }) => {
   return (
     <div className="directory-container">
       {categories.map((category) => {
-        return <CategoryItem key={category.id} category={category} />;
+        return <CategoryItem />;
       })}
     </div>
   );
