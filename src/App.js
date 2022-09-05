03619d6573db88809e17fcb51ee3e3dc11219b4b
MD AL JABIR <aljabir098@gmail.com>
2022-09-05 22:16:14 +0600

    navbar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home";
+import Navigation from "./routes/navigation/navigation";
 
 const Shop = () => {
   return <h1>I will be developer</h1>;
@@ -8,7 +9,8 @@ const Shop = () => {
 const App = () => {
   return (
     <Routes>
-      <Route path="/home" element={<Home />}>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
       </Route>
     </Routes>
