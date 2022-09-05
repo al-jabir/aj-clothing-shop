1e45384ef97a982f513ded55a073c48971f1b27e
MD AL JABIR <aljabir098@gmail.com>
2022-09-05 21:56:46 +0600

    outlets

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home";
 
+const Shop = () => {
+  return <h1>I will be developer</h1>;
+};
+
 const App = () => {
-  return <Home />;
+  return (
+    <Routes>
+      <Route path="/home" element={<Home />}>
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
 };
 
 export default App;
--- src/routes/home/home.jsx
+++ src/routes/home/home.jsx
@@ -1,3 +1,4 @@
+import { Outlet } from "react-router-dom";
 import Directory from "../../components/directory/directory.component";
 
 const Home = () => {
@@ -33,7 +34,12 @@ const Home = () => {
     },
   ];
 
-  return <Directory categories={categories} />;
+  return (
+    <>
+      <Directory categories={categories} />
+      <Outlet />
+    </>
+  );
 };
 
 export default Home;
