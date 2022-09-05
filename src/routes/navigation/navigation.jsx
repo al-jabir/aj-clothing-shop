c20ab551906c12e1480e3ca6b10cfbb79b330ad9
MD AL JABIR <aljabir098@gmail.com>
2022-09-05 22:58:59 +0600

    something

--- src/routes/navigation/navigation.jsx
+++ src/routes/navigation/navigation.jsx
@@ -1,12 +1,13 @@
 import { Fragment } from "react";
 import { Link, Outlet } from "react-router-dom";
+import "./navigation.scss";
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-link" to="/">
-          <p>Logo</p>
+          <p>AJ SHOP</p>
         </Link>
         <div className="nav-container">
           <Link className="nav-links" to="/shop">
