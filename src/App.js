6dbd3a27e037142e115149c62d346194c9604905
MD AL JABIR <aljabir098@gmail.com>
2022-09-06 03:21:54 +0600

    signin

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./routes/home/home";
 import Navigation from "./routes/navigation/navigation";
+import SignIn from "./routes/sign-in/SignIn";
 
 const Shop = () => {
   return <h1>I will be developer</h1>;
@@ -12,6 +13,7 @@ const App = () => {
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
         <Route path="shop" element={<Shop />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
