2ab787658929cb9832c859e24e52ed1e6d579531
MD AL JABIR <aljabir098@gmail.com>
2022-09-06 04:03:55 +0600

    firebase added

--- src/routes/sign-in/SignIn.jsx
+++ src/routes/sign-in/SignIn.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { singInWithGooglePopup } from "../../utils/firebase/firebase";
 
 const SignIn = () => {
+  const logGoolgeUser = async () => {
+    const res = await singInWithGooglePopup();
+    console.log(res);
+  };
   return (
     <div>
       <h1>I am sign page</h1>
+      <button onClick={logGoolgeUser}>Sign in with Google</button>
     </div>
   );
 };
---
+++ src/utils/firebase/firebase.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB6T4CmSA0_nwa0ulekVdOqsPsGs5q4ito",
+  authDomain: "aj-clothing-shop.firebaseapp.com",
+  projectId: "aj-clothing-shop",
+  storageBucket: "aj-clothing-shop.appspot.com",
+  messagingSenderId: "144779612794",
+  appId: "1:144779612794:web:ff7d68352e9644b00b61c3",
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+provider.setCustomParameters({
+  prompt: "select_account",
+});
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+  console.log(userAuth);
+};
+
+export const auth = getAuth();
+export const singInWithGooglePopup = () => signInWithPopup(auth, provider);
