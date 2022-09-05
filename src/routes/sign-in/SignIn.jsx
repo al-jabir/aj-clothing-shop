6b21e73ec2e1ec559190c5b095f0a90bbe3791d8
MD AL JABIR <aljabir098@gmail.com>
2022-09-06 03:24:00 +0600

    added style and Ok

--- src/routes/sign-in/SignIn.jsx
+++ src/routes/sign-in/SignIn.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <div>
+      <h1>I am sign page</h1>
+    </div>
+  );
 };
 
 export default SignIn;
