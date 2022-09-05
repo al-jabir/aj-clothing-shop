7139af844779a9234b6b92cab023d2dd698dfefc
MD AL JABIR <aljabir098@gmail.com>
2022-09-05 23:01:44 +0600

    add logo

--- src/routes/navigation/navigation.jsx
+++ src/routes/navigation/navigation.jsx
@@ -1,13 +1,13 @@
 import { Fragment } from "react";
 import { Link, Outlet } from "react-router-dom";
+import { ReactComponent as Ajshop } from "../../assets/aj.svg";
 import "./navigation.scss";
-
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-link" to="/">
-          <p>AJ SHOP</p>
+          <Ajshop className="logo" />
         </Link>
         <div className="nav-container">
           <Link className="nav-links" to="/shop">
