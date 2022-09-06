f7203ac5a0b911f4f707eda675ad4150a0191e18
MD AL JABIR <aljabir098@gmail.com>
2022-09-06 20:53:43 +0600

    Done

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -28,9 +28,9 @@ export const db = getFirestore();
 
 export const createUserDocumentFromAuth = async (userAuth) => {
   const userDocRef = doc(db, "users", userAuth.uid);
-  console.log(userDocRef);
+  // console.log(userDocRef);
   const userSnapshot = await getDoc(userDocRef);
-  console.log(userSnapshot);
+  // console.log(userSnapshot);
 
   if (!userSnapshot.exists()) {
     const { displayName, email } = userAuth;
