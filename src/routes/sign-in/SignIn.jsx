9fc9fd2276d09d121137cd12c82d70e403ac7eee
MD AL JABIR <aljabir098@gmail.com>
2022-09-06 16:04:37 +0600

    fixed firebase

--- src/routes/sign-in/SignIn.jsx
+++ src/routes/sign-in/SignIn.jsx
@@ -7,6 +7,7 @@ const SignIn = () => {
   const logGoogleUser = async () => {
     const { user } = await signInWithGooglePopup();
     const userDocRef = await createUserDocumentFromAuth(user);
+    console.log(userDocRef);
   };
 
   return (
