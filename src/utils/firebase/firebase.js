1f9fcca28f3649f94d2e9b2c07d8c568e78b49a3
al-jabir <aljabir098@gmail.com>
2022-10-01 18:48:08 +0600

    initial commit

--- src/utils/firebase/firebase.js
+++ src/utils/firebase/firebase.js
@@ -3,6 +3,7 @@ import {
   createUserWithEmailAndPassword,
   getAuth,
   GoogleAuthProvider,
+  onAuthStateChanged,
   signInWithEmailAndPassword,
   signInWithPopup,
   signInWithRedirect,
@@ -79,3 +80,6 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 };
 
 export const signOutUser = async () => await signOut(auth);
+
+export const onAuthStateChangedListener = (callback) =>
+  onAuthStateChanged(auth, callback);
