49200a7351a7cdf55c7888c99791c802022c96ba
al-jabir <aljabir098@gmail.com>
2022-12-03 10:54:51 +0600

    added style sass

--- src/routes/shop/shop.jsx
+++ src/routes/shop/shop.jsx
@@ -2,10 +2,12 @@ import React, { useContext } from "react";
 import ProductCard from "../../components/product-card/ProductCard";
 import { ProductsContext } from "../../contexts/productContext";
 
+import "./shop.scss";
+
 const Shop = () => {
   const { products } = useContext(ProductsContext);
   return (
-    <div>
+    <div className="products-container">
       {products.map((product) => (
         <ProductCard key={product.id} product={product} />
       ))}
