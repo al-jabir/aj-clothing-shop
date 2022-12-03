dfec7f775addd8527a6604e5679a2ce9661d216f
al-jabir <aljabir098@gmail.com>
2022-12-03 13:25:22 +0600

    adding cart context mapping data

--- src/components/cart-dropdown/CartDropDown.jsx
+++ src/components/cart-dropdown/CartDropDown.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import "./cart-dropdown.scss";
 
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cartContext";
 import Button from "../button/button";
+import CartItem from "../cart-item/CartItems";
 
 const CartDropDown = () => {
+  const { cartItems } = useContext(CartContext);
   return (
     <div className="cart-dropdown-container">
       <div className="cart-items">
-        <Button>GO TO CHECKOOUT</Button>
+        {cartItems.map((item, idx) => (
+          <CartItem key={idx} cartItem={item} />
+        ))}
       </div>
+      <Button>GO TO CHECKOOUT</Button>
     </div>
   );
 };
